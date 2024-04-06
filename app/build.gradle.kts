676923a773b5d017ecb6c490aa6f43043a34f6f2
Jagadeesh <jagadeesh.k.2802@gmail.com>
2024-04-06 14:16:09 +0530

    refactor: - Version Code Changed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         applicationId = "com.jackappsdev.password_manager"
         minSdk = 28
         targetSdk = 34
-        versionCode = 3
+        versionCode = 4
         versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"

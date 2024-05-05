02d591c62c74453e998b743612f1869f93f4b1f7
Jagadeesh <jagadeesh.k.2802@gmail.com>
2024-05-05 10:11:28 +0530

    fix: - Removed dead code from merge

--- app/src/main/java/com/jackappsdev/password_manager/presentation/main/MainActivity.kt
+++ app/src/main/java/com/jackappsdev/password_manager/presentation/main/MainActivity.kt
@@ -2,56 +2,24 @@ package com.jackappsdev.password_manager.presentation.main
 
 import android.os.Bundle
 import android.view.WindowManager
-import android.widget.Toast
 import androidx.activity.compose.setContent
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.fragment.app.FragmentActivity
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.compose.rememberNavController
-import com.google.android.play.core.appupdate.AppUpdateManager
-import com.google.android.play.core.appupdate.AppUpdateManagerFactory
-import com.google.android.play.core.appupdate.AppUpdateOptions
-import com.google.android.play.core.install.InstallStateUpdatedListener
-import com.google.android.play.core.install.model.AppUpdateType
-import com.google.android.play.core.install.model.InstallStatus
-import com.google.android.play.core.install.model.UpdateAvailability
-import com.google.android.play.core.ktx.isFlexibleUpdateAllowed
-import com.google.android.play.core.ktx.isImmediateUpdateAllowed
-import com.jackappsdev.password_manager.R
 import com.jackappsdev.password_manager.presentation.navigation.Router
 import com.jackappsdev.password_manager.presentation.screens.password_lock.PasswordLockViewModel
 import com.jackappsdev.password_manager.presentation.theme.PasswordManagerTheme
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlin.time.Duration.Companion.seconds
 
 @AndroidEntryPoint
 class MainActivity : FragmentActivity() {
-    private lateinit var appUpdateManager: AppUpdateManager
     private val passwordLockViewModel: PasswordLockViewModel by viewModels()
     private val mainViewModel: MainViewModel by viewModels()
-
-    private val installStateUpdatedListener = InstallStateUpdatedListener { installState ->
-        if (installState.installStatus() == InstallStatus.DOWNLOADED) {
-            Toast.makeText(this, getString(R.string.toast_app_update_msg), Toast.LENGTH_SHORT)
-                .show()
-
-            lifecycleScope.launch {
-                delay(5.seconds)
-                appUpdateManager.completeUpdate()
-            }
-        }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val splashScreen = installSplashScreen()
-        appUpdateManager = AppUpdateManagerFactory.create(this)
-        appUpdateManager.registerListener(installStateUpdatedListener)
-        checkForAppUpdates()
 
         splashScreen.setKeepOnScreenCondition {
             passwordLockViewModel.state.hasPasswordSet == null && mainViewModel.useDynamicColors == null
@@ -69,47 +37,5 @@ class MainActivity : FragmentActivity() {
                 Router(navController, passwordLockViewModel)
             }
         }
-    }
-
-    private fun checkForAppUpdates() {
-        val appUpdateInfoTask = appUpdateManager.appUpdateInfo
-
-        appUpdateInfoTask.addOnSuccessListener { info ->
-            val isUpdateAvailable = info.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
-            val isUpdateAllowed = info.isFlexibleUpdateAllowed || info.isImmediateUpdateAllowed
-
-            val launcher = registerForActivityResult(
-                ActivityResultContracts.StartIntentSenderForResult()
-            ) { activityResult ->
-                if (activityResult.resultCode != RESULT_OK) {
-                    Toast.makeText(
-                        this,
-                        getString(R.string.toast_update_failed),
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
-            }
-
-            if (isUpdateAvailable && isUpdateAllowed) {
-                if ((info.clientVersionStalenessDays() ?: 0) > 14) {
-                    appUpdateManager.startUpdateFlowForResult(
-                        info,
-                        launcher,
-                        AppUpdateOptions.newBuilder(AppUpdateType.IMMEDIATE).build()
-                    )
-                } else {
-                    appUpdateManager.startUpdateFlowForResult(
-                        info,
-                        launcher,
-                        AppUpdateOptions.newBuilder(AppUpdateType.FLEXIBLE).build()
-                    )
-                }
-            }
-        }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        appUpdateManager.unregisterListener(installStateUpdatedListener)
     }
 }

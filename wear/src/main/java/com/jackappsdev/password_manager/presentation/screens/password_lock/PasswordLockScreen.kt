e36f172d8f3ed44bad1b2053d003f14a1a8238e9
Jagadeesh <jagadeesh.k.2802@gmail.com>
2024-05-22 21:31:36 +0530

    feat: - PasswordLockScreen is now responsive on Wear OS

--- wear/src/main/java/com/jackappsdev/password_manager/presentation/screens/password_lock/PasswordLockScreen.kt
+++ wear/src/main/java/com/jackappsdev/password_manager/presentation/screens/password_lock/PasswordLockScreen.kt
@@ -1,5 +1,6 @@
 package com.jackappsdev.password_manager.presentation.screens.password_lock
 
+import android.graphics.Color.BLACK
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -17,6 +18,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
@@ -29,6 +31,7 @@ import androidx.wear.compose.material.Text
 import androidx.wear.compose.material.TimeText
 import androidx.wear.compose.material.Vignette
 import androidx.wear.compose.material.VignettePosition
+import com.jackappsdev.password_manager.presentation.theme.PasswordManagerTheme
 import com.jackappsdev.password_manager.presentation.theme.pagePadding
 
 @Composable
@@ -48,130 +51,165 @@ fun PasswordLockScreen() {
 
             Text(
                 "••••",
-                fontSize = 18.sp,
+                fontSize = 20.sp,
                 textAlign = TextAlign.Center,
                 letterSpacing = TextUnit(2.5f, TextUnitType.Sp)
             )
 
-            Spacer(modifier = Modifier.height(4.dp))
+            Spacer(modifier = Modifier.height(8.dp))
 
             Row(
                 horizontalArrangement = Arrangement.SpaceEvenly,
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxWidth()
             ) {
                 Chip(
                     onClick = {},
-                    modifier = Modifier.size(30.dp),
+                    modifier = Modifier.weight(0.5f),
                     colors = ChipDefaults.chipColors(backgroundColor = Color.Transparent),
+                    contentPadding = PaddingValues(horizontal = 18.dp),
                     label = { Text("1") }
                 )
 
                 Chip(
                     onClick = {},
-                    modifier = Modifier.size(30.dp),
+                    modifier = Modifier.weight(0.5f),
                     colors = ChipDefaults.chipColors(backgroundColor = Color.Transparent),
+                    contentPadding = PaddingValues(horizontal = 18.dp),
                     label = { Text("2") }
                 )
 
                 Chip(
                     onClick = {},
-                    modifier = Modifier.size(30.dp),
+                    modifier = Modifier.weight(0.5f),
                     colors = ChipDefaults.chipColors(backgroundColor = Color.Transparent),
+                    contentPadding = PaddingValues(horizontal = 18.dp),
                     label = { Text("3") }
                 )
 
                 Chip(
                     onClick = {},
-                    modifier = Modifier.size(30.dp),
+                    modifier = Modifier.weight(0.5f),
                     colors = ChipDefaults.chipColors(backgroundColor = Color.Transparent),
+                    contentPadding = PaddingValues(horizontal = 12.dp),
                     label = { },
-                    contentPadding = PaddingValues(8.dp),
                     icon = {
-                        Icon(Icons.AutoMirrored.Default.Backspace, contentDescription = "Delete")
+                        Icon(
+                            Icons.AutoMirrored.Default.Backspace,
+                            modifier = Modifier.size(16.dp),
+                            contentDescription = "Delete"
+                        )
                     }
                 )
             }
 
             Row(
                 horizontalArrangement = Arrangement.SpaceEvenly,
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxWidth()
             ) {
                 Chip(
                     onClick = {},
-                    modifier = Modifier.size(30.dp),
+                    modifier = Modifier.weight(0.5f),
                     colors = ChipDefaults.chipColors(backgroundColor = Color.Transparent),
+                    contentPadding = PaddingValues(horizontal = 18.dp),
                     label = { Text("4") }
                 )
 
                 Chip(
                     onClick = {},
-                    modifier = Modifier.size(30.dp),
+                    modifier = Modifier.weight(0.5f),
                     colors = ChipDefaults.chipColors(backgroundColor = Color.Transparent),
+                    contentPadding = PaddingValues(horizontal = 18.dp),
                     label = { Text("5") }
                 )
 
                 Chip(
                     onClick = {},
-                    modifier = Modifier.size(30.dp),
+                    modifier = Modifier.weight(0.5f),
                     colors = ChipDefaults.chipColors(backgroundColor = Color.Transparent),
+                    contentPadding = PaddingValues(horizontal = 18.dp),
                     label = { Text("6") }
                 )
 
                 Chip(
                     onClick = {},
-                    modifier = Modifier.size(30.dp),
+                    modifier = Modifier.weight(0.5f),
                     colors = ChipDefaults.chipColors(backgroundColor = Color.Transparent),
+                    contentPadding = PaddingValues(horizontal = 12.dp),
                     label = { },
-                    contentPadding = PaddingValues(8.dp),
                     icon = {
-                        Icon(Icons.Default.Done, contentDescription = "Done")
+                        Icon(
+                            Icons.Default.Done,
+                            modifier = Modifier.size(16.dp),
+                            contentDescription = "Done"
+                        )
                     }
                 )
             }
 
             Row(
                 horizontalArrangement = Arrangement.SpaceEvenly,
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxWidth()
             ) {
                 Chip(
                     onClick = {},
-                    modifier = Modifier.size(30.dp),
+                    modifier = Modifier.weight(0.5f),
                     colors = ChipDefaults.chipColors(backgroundColor = Color.Transparent),
+                    contentPadding = PaddingValues(horizontal = 18.dp),
                     label = { Text("7") }
                 )
 
                 Chip(
                     onClick = {},
-                    modifier = Modifier.size(30.dp),
+                    modifier = Modifier.weight(0.5f),
                     colors = ChipDefaults.chipColors(backgroundColor = Color.Transparent),
+                    contentPadding = PaddingValues(horizontal = 18.dp),
                     label = { Text("8") }
                 )
 
                 Chip(
                     onClick = {},
-                    modifier = Modifier.size(30.dp),
+                    modifier = Modifier.weight(0.5f),
                     colors = ChipDefaults.chipColors(backgroundColor = Color.Transparent),
+                    contentPadding = PaddingValues(horizontal = 18.dp),
                     label = { Text("9") }
                 )
 
-                Spacer(modifier = Modifier.size(30.dp))
+                Spacer(modifier = Modifier.weight(0.5f))
             }
 
             Row(
                 horizontalArrangement = Arrangement.Center,
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxWidth()
             ) {
-                Spacer(modifier = Modifier.size(30.dp))
+                Spacer(modifier = Modifier.weight(1f))
 
                 Chip(
                     onClick = {},
-                    modifier = Modifier.size(30.dp),
+                    modifier = Modifier.weight(1f),
                     colors = ChipDefaults.chipColors(backgroundColor = Color.Transparent),
+                    contentPadding = PaddingValues(horizontal = 18.dp),
                     label = { Text("0") }
                 )
 
-                Spacer(modifier = Modifier.size(70.dp))
+                Spacer(modifier = Modifier.weight(2f))
             }
         }
+    }
+}
+
+@Preview(device = "id:wearos_small_round", showBackground = true, backgroundColor = BLACK.toLong())
+@Preview(device = "id:wearos_large_round", showBackground = true, backgroundColor = BLACK.toLong())
+@Composable
+fun PasswordLockScreenPreviewSmall() {
+    PasswordManagerTheme {
+        PasswordLockScreen()
     }
 }

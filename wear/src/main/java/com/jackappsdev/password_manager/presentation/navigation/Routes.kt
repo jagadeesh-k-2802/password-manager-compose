f8437238efc3a1437f99e5650fe82ab71bea649a
Jagadeesh K <jagadeesh.k.2802@gmail.com>
2025-04-20 08:10:05 +0530

    Update Routes.kt

--- wear/src/main/java/com/jackappsdev/password_manager/presentation/navigation/Routes.kt
+++ wear/src/main/java/com/jackappsdev/password_manager/presentation/navigation/Routes.kt
@@ -2,12 +2,15 @@ package com.jackappsdev.password_manager.presentation.navigation
 
 sealed class Graph(val route: String) {
     data object LockGraph : Graph("lock")
+
     data object UnlockedGraph : Graph("unlocked")
 }
 
 sealed class Routes(val route: String) {
     data object PasswordLock : Routes("password-lock")
+
     data object Home : Routes("home")
+
     data object PasswordItemDetail : Routes("password-item-detail/{id}") {
         operator fun invoke(id: Int) = route.replace("{id}", id.toString())
     }
